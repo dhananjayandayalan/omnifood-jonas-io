bd54c1ba5e55f63562818f510a822ca4943b406f
Dhananjayan DV <dhananjayan.dayalan@avizva.com>
2022-06-21 12:54:32 +0530

    Button Functionality added with javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+const btnNav = document.querySelector('.btn-mobile-nav');
+
+const header = document.querySelector('.header');
+
+btnNav.addEventListener('click', () => {
+    header.classList.toggle("nav-open");
+});
+
 const checkFlexGap = () => {
   let flex = document.createElement("div");
   flex.style.display = "flex";
